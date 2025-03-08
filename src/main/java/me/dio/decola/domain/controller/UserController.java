0b3631291c2d593e2dcba5a92205597ee745ac71
LineteDantas <linetedantas.ads@gmail.com>
2025-03-08 19:33:21 -0300

    Implementação da Camada de Dominio

---
+++ src/main/java/me/dio/decola/domain/controller/UserController.java
@@ -0,0 +1,37 @@
+package me.dio.decola.domain.controller;
+
+import me.dio.decola.domain.model.User;
+import me.dio.decola.domain.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        return ResponseEntity.ok(userService.findById(id));
+    }
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user){
+        var userCreated = userService.create(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreated.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
+    }
+
+}
